5ef1a51820b770b1f591a43fc3644b634fc713db
Zharova Yulya <zharovayulya1997@gmail.com>
2023-08-08 20:28:49 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args)
+        {
+            int cost = 13676;
+            int rate  = 20;
+            int bonus  = cost/rate;
+        System.out.println(bonus);
+    }
+}
