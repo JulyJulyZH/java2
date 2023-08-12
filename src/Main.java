0cf51d20d213a9f226d2e59ed7226194eeef8e6e
Zharova Yulya <zharovayulya1997@gmail.com>
2023-08-12 14:19:42 +0500

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-        public static void main(String[] args)
-        {
-            int cost = 13676;
-            int rate  = 20;
-            int bonus  = cost/rate;
+    public static void main(String[] args) {
+        int cost = 13676;
+        int rate = 20;
+        int bonus = cost / rate;
         System.out.println(bonus);
     }
 }
